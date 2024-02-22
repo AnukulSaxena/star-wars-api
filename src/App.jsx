ab00199e878e70bf784ad44685b5484e707f28da
Anukul Prakash Saxena <anukulsaxena83@gmail.com>
2024-02-22 16:13:25 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <div className=" w-full h-screen flex justify-center items-center px-2 bg-neutral-800 text-white">HELLO WORLD</div>
+  )
+}
+
+export default App
